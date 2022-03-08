d2a0caa9b02defc07f52b65d274deb1e0b1b068f
krystalgamer <callofdutypsp@gmail.com>
2022-03-08 20:01:02 +0000

    more consistent login bypass

--- stem_emulator.user.js
+++ stem_emulator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stem Player Emulator
 // @namespace    https://www.stemplayer.com/
-// @version      0.8.1
+// @version      0.8.2
 // @description  Emulator for Kanye West's stem player
 // @author       krystalgamer
 // @match        https://www.stemplayer.com/*
@@ -645,6 +645,6 @@ console.log('out maquina');
         });
     }
 
-    window.localStorage.setItem('production_sp_basic_session', JSON.stringify({"AuthToken":"YWxleEBrYW5vLm1lOjY5Njk2OTY5Njk2OTY5Njk="}))
+    window.localStorage.setItem('production_sp_basic_session', JSON.stringify({"AuthToken":btoa(atob('QUxFWEBLQU5PLk1F')+':'+Date.now())}))
 
 })();
